115f35373f6af984e154a02805eef77abf13ce10
Taqi Muhammad <taqi.rohan109@gmail.com>
2024-10-09 18:06:34 +0500

    added react icons

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,16 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import HomeCards from './components/HomeCards';
 import JobListings from './components/JobListings';
+import ViewJobs from './components/ViewJobs';
 
 const App = () => {
   return (
     <>
-    <Navbar />
-    <Hero />
-    <HomeCards />
-    <JobListings />
-
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
-
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewJobs />
     </>
   )
 };
