80a26cdc186b01607e047a23a1d7507814ebe8cc
Taqi Muhammad <taqi.rohan109@gmail.com>
2024-12-02 17:24:33 +0500

    added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewJobs from './components/ViewJobs';
+import {Route,
+        createBrowserRouter,
+        createRoutesFromElements,
+        RouterProvider
+} from 'react-router-dom';
+import React from 'react';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path='/' element={<MainLayout />}>
+    <Route index element={<HomePage />} />
+    <Route path='/jobs' element={<JobsPage />}/>
+    <Route path='*' element={<NotFoundPage />}/>  
+  </Route >
+  )
+);
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewJobs />
-    </>
-  )
+  return <RouterProvider router={router} />;
 };
 
 export default App;
